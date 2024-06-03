f7c7480cb34f8ff4f7b9957fb97b73e07ef4a2cd
andreamartinez16c <andrea.martinez16.c@gmail.com>
2024-06-03 03:31:19 +0200

    commit

--- MvcPersonajesExamenTemplate/Program.cs
+++ MvcPersonajesExamenTemplate/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddTransient<ServiceStorageAWS>();
 builder.Services.AddTransient<ServiceApiPersonajes>();
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
